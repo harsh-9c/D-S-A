f920dd7a5cbb792359247bd471b31d58a990548a
Harsh Jangitwar <78642411+harsh-9c@users.noreply.github.com>
2022-07-07 16:01:23 +0530

    Create Merge_Two_SortedArrays.cpp

---
+++ Merge_Two_SortedArrays.cpp
@@ -0,0 +1,19 @@
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        int i=m-1;
+        int j=n-1;
+        int k=n+m-1;
+        
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                k-=1; i-=1;
+            }
+            else{
+                nums1[k]=nums2[j];
+                k-=1; j-=1;
+            }
+        }
+        
+        
+    }
