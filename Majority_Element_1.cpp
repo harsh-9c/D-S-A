39547c6c50b72982f8ee41f0190f6dfa5c575d6f
Harsh Jangitwar <78642411+harsh-9c@users.noreply.github.com>
2022-07-08 14:03:02 +0530

    Create Majority_Element_1.cpp

---
+++ Majority_Element_1.cpp
@@ -0,0 +1,16 @@
+int majorityElement(vector<int>& nums) {
+        
+        int major=nums[0];
+        int count=1;
+        for(int i=1;i<nums.size();i++){
+          
+            if(nums[i]==major) count++;
+            else if(nums[i]!=major) count--;
+            if(count==0){
+                major=nums[i]; count=1;
+            }     
+        }
+        
+        return major;
+        
+    }
