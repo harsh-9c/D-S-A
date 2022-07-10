3f8d8049878768d10a6fc364477decf54180b5c9
Harsh Jangitwar <78642411+harsh-9c@users.noreply.github.com>
2022-07-10 19:40:19 +0530

    Update Set_Matrix_Zeores.cpp

--- Set_Matrix_Zeores.cpp
+++ Set_Matrix_Zeores.cpp
@@ -1,21 +1,25 @@
-void setZeroes(vector<vector<int>>& matrix) {
+ void setZeroes(vector<vector<int>>& matrix) {
         
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<pair<int,int>>v;
+        
+        bool col0=true;
         
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    v.push_back({i,j});
+            if(matrix[i][0]==0) col0=false;
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]==0){ 
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
         
-        for(auto x:v){
-            int i=x.first,j=x.second;
-            for(int k=0;k<m;k++) matrix[i][k]=0;
-            for(int k=0;k<n;k++) matrix[k][j]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=1;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;   
+            }
+            if(col0==false) matrix[i][0]=0;
         }
-           
+        
     }
