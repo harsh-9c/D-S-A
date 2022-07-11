808d62ef0d308462c9b636270372004086b967c9
Harsh Jangitwar <78642411+harsh-9c@users.noreply.github.com>
2022-07-11 18:33:35 +0530

    Create Reverse_LL.cpp

---
+++ Linked List/Reverse_LL.cpp
@@ -0,0 +1,14 @@
+//  ITERATIVE METHOD
+
+ListNode* reverseList(ListNode* head) {
+        
+        ListNode *nextNode, *prevNode = NULL;
+        while (head) {
+            nextNode = head->next;
+            head->next = prevNode;
+            prevNode = head;
+            head = nextNode;
+        }
+        return prevNode;
+       
+    }
