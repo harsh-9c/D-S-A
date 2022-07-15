6d6ec4b6e1123921fa02e0dd0c9e258b3e344821
Harsh Jangitwar <78642411+harsh-9c@users.noreply.github.com>
2022-07-15 12:53:43 +0530

    Update Remove_Nth_Node.cpp

--- Linked List/Remove_Nth_Node.cpp
+++ Linked List/Remove_Nth_Node.cpp
@@ -1,34 +1,13 @@
 ListNode* removeNthFromEnd(ListNode* head, int n) {
         
+        ListNode *fast=head,*slow=head;
         
-        ListNode* temp=head;
-      
-        int cnt=0;
-        while(temp!=NULL){
-            cnt++;
-            temp=temp->next;
-        }
+        for(int i=0;i<n;i++) fast=fast->next;
         
-        if(cnt==1) return NULL;
-        if(cnt==n) return head->next;
+        if(!fast) return head->next;
         
-        
-        int tar=cnt-n+1;
-        
-        temp=head->next;
-        ListNode* prev=head;
-        
-        tar--;
-        
-        while(temp){
-            tar--;
-            if(tar==0){
-                prev->next=temp->next;
-                break;
-            }
-            prev=temp;
-            temp=temp->next;
-        }
+        while (fast->next) fast=fast->next,slow=slow->next;
+        slow->next=slow->next->next;
         
         return head;
         
