465891f5edb016258ec602a84392f77946ff33ad
Harsh Jangitwar <78642411+harsh-9c@users.noreply.github.com>
2022-07-15 13:00:27 +0530

    Update Remove_Nth_Node.cpp

--- Linked List/Remove_Nth_Node.cpp
+++ Linked List/Remove_Nth_Node.cpp
@@ -1,3 +1,49 @@
+// IT's TAKING 2 PASSES
+
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        
+        ListNode* temp=head;
+        ListNode* temp2;
+        int cnt=0;
+        while(temp!=NULL){
+            cnt++;
+            temp=temp->next;
+        }
+        
+        if(cnt==1) return NULL;
+        if(cnt==n) return head->next;
+        
+        
+        int tar=cnt-n+1;
+        
+        temp=head->next;
+        ListNode* prev=head;
+        
+        tar--;
+        
+        while(temp!=NULL){
+            tar--;
+            if(tar==0){
+                prev->next=temp->next;
+                break;
+            }
+            prev=temp;
+            temp=temp->next;
+        }
+        
+        return head;
+        
+        
+        
+    }
+
+
+
+
+// IT's TAKING ONLY SINGLE PASS
+
+
 ListNode* removeNthFromEnd(ListNode* head, int n) {
         
         ListNode *fast=head,*slow=head;
