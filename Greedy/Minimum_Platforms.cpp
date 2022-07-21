37a51559a68c9803846bbb45da749d63d6481822
Harsh Jangitwar <78642411+harsh-9c@users.noreply.github.com>
2022-07-21 13:22:16 +0530

    Update Minimum_Platforms.cpp

--- Greedy/Minimum_Platforms.cpp
+++ Greedy/Minimum_Platforms.cpp
@@ -1,17 +1,19 @@
  int findPlatform(int arr[], int dep[], int n){
         
-       sort(arr,arr+n);
-       sort(dep,dep+n);
-       
-       int ans=1; 
-       int idx1=0,idx2=1;
-       
-       for(int i=1;i<n;i++){
-           if(dep[idx1]<arr[idx2]) idx1++,idx2++;
-           else ans++,idx2++;
-       }
+        sort(arr,arr+n);
+        sort(dep,dep+n);
+        
+        int ans=1;
+        int prev_idx=0; 
+        for(int i=1;i<n;i++){
+            if(arr[i]>dep[prev_idx]){
+                prev_idx++;
+            }
+            else{
+                ans++;
+            }
+        }
         
         return ans;
-    
-    
+        
     }
