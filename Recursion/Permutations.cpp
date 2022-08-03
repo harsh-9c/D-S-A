ab0eea5b31d4d944010f3fa9ae1ffabff264e7bc
Harsh Jangitwar <78642411+harsh-9c@users.noreply.github.com>
2022-08-03 12:18:25 +0530

    Update Permutations.cpp

--- Recursion/Permutations.cpp
+++ Recursion/Permutations.cpp
@@ -24,3 +24,29 @@
         return ans;
         
     }
+
+
+// Optimized Apporoach in terms of space by swaping the nos
+
+void solve(vector<vector<int>>&ans,vector<int>&nums,int idx){
+        
+        if(idx==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+       
+        for(int i=idx;i<nums.size();i++){ 
+           swap(nums[i],nums[idx]);
+            solve(ans,nums,idx+1);
+            swap(nums[i],nums[idx]);
+        }
+        
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        vector<vector<int>>ans;
+        vector<int>v;
+        solve(ans,nums,0);
+        return ans;
+        
+    }
