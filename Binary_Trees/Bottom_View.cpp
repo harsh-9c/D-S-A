ce6b3d0379252470af41fd03ca47cdc4155abddf
Harsh Jangitwar <78642411+harsh-9c@users.noreply.github.com>
2022-08-04 15:57:19 +0530

    Create Bottom_View.cpp

---
+++ Binary_Trees/Bottom_View.cpp
@@ -0,0 +1,28 @@
+vector<int> leftView(Node *root){
+    
+    vector<int>ans;
+    
+    if(!root) return ans;
+    
+    queue<pair<Node*,int>>q;
+    q.push({root,0}); 
+    map<int,int>m; int level=0;
+    
+    while(!q.empty()){
+        
+        pair<Node*,int>node =q.front();
+        q.pop();
+        
+        m[node.second]=node.first->data;
+        
+
+        if(node.first->left) q.push({node.first->left,node.second-1});
+        if(node.first->right) q.push({node.first->right,node.second+1});
+        
+    }
+    
+    for(auto x:m) ans.push_back(x.second);
+    
+    return ans;
+   
+}
