f7705bbc6392ecc0d5e4b7fa79e50b4945f6e140
Harsh Jangitwar <78642411+harsh-9c@users.noreply.github.com>
2022-08-07 14:58:38 +0530

    Create Identical_Or_Not.cpp

---
+++ Binary_Trees/Identical_Or_Not.cpp
@@ -0,0 +1,5 @@
+bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p) return !q;
+        if(!q) return !p;
+        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
